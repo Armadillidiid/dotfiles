ac65678510d504205171f7ba166ad17ea624c048
Emmanuel Isenah <emmanuelisenah@gmail.com>
2023-04-28 19:49:41 +0100

    Added shit ton of keymaps

--- LazyVim/.config/LazyVim/lua/config/keymaps.lua
+++ LazyVim/.config/LazyVim/lua/config/keymaps.lua
@@ -3,6 +3,7 @@
 -- Add any additional keymaps here
 
 local Util = require("lazyvim.util")
+local wk = require("which-key")
 
 local function map(mode, lhs, rhs, opts)
   local keys = require("lazy.core.handler").handlers.keys
@@ -24,38 +25,63 @@ map("n", "<C-p>", Util.telescope("files"))
 -- Map "Ctrl+n" to Telescope Recent files
 map("n", "<c-n>", "<cmd>Telescope oldfiles<cr>")
 
--- Toggle auto-save
-map("n", "<leader>n", ":ASToggle<CR>", { desc = "Toggle auto-save" })
+-- -- Toggle auto-save
+-- map("n", "<leader>n", ":ASToggle<CR>", { desc = "Toggle auto-save" })
 
 --n  Map <leader><space> to delete buffer
-map("n", "<leader><space>", ':lua require("mini.bufremove").delete(0, false)<CR>', { desc = "Delete buffer" })
+-- map("n", "<leader><space>", ':lua require("mini.bufremove").delete(0, false)<CR>', { desc = "Delete buffer" })
 
 -- Telescope project_root
-map("n", "<leader>pP", ":lua require'telescope'.extensions.project.project{}<CR>", { desc = "Telescope project",  noremap = true, silent = true })
+map(
+  "n",
+  "<leader>pP",
+  ":lua require'telescope'.extensions.project.project{}<CR>",
+  { desc = "Telescope project", noremap = true, silent = true }
+)
 
 -- ProjectMgr
 map("n", "<leader>pp", ":ProjectMgr<CR>", { desc = "ProjectMgr" })
 
--- ChatGPT mapping to open chat 
+-- ChatGPT mapping to open chat
 map("n", "<leader>cc", ":ChatGPT<CR>", { desc = "ChatGPT" })
 
 -- Find bugs with code using codeGPT
 map("v", "<leader>cp", ":Chat please fix any bugs and errors in the following code<CR>", { desc = "ChatGPTProblem" })
 map("x", "<leader>cp", ":Chat please fix any bugs and errors in the following code<CR>", { desc = "ChatGPTProblem" })
 
--- nvim-surround
--- vim.g["surround_no_mappings"] = 1
--- vim.keymap.set("n", "ds", "<Plug>Dsurround")
--- map("n", "cs", "<Plug>Csurround")
--- map("n", "cS", "<Plug>CSurround")
--- map("n", "ys", "<Plug>Ysurround")
--- map("n", "yS", "<Plug>YSurround")
--- map("n", "yss", "<Plug>Yssurround")
--- map("n", "ySs", "<Plug>YSsurround")
--- map("n", "ySS", "<Plug>YSsurround")
---
--- -- The conflicting ones. Note that `<Plug>(leap-cross-window)`
--- -- _does_ work in Visual mode, if jumping to the same buffer,
--- -- so in theory, `gs` could be useful for Leap too...
--- map("x", "gs", "<Plug>VSurround")
--- map("x", "gS", "<Plug>VgSurround")
+-- Leetcode keymaps
+map("n", "<leader>Ll", ":LeetCodeList<cr>", { desc = "LeetCodeList" })
+map("n", "<leader>Lt", ":LeetCodeTest<cr>", { desc = "LeetCodeTest" })
+map("n", "<leader>Ls", ":LeetCodeSubmit<cr>", { desc = "LeetCodeSubmit" })
+map("n", "<leader>Li", ":LeetCodeSignIn<cr>", { desc = "LeetCodeSignIn" })
+
+-- Telescope browse
+map("n", "<leader>fB", ":lua require 'telescope'.extensions.file_browser.file_browser()<CR>", { desc = "File browse" })
+
+-- Navbuddy
+map("n", "<leader>nv", ":Navbuddy<CR>", { desc = "Navbuddy" })
+
+-- Copilot
+wk.register({
+  ["<leader>cC"] = {
+    name = "Copilot",
+    d = {":Copilot disable<CR>", "disable"},
+    e = {":Copilot enable<CR>", "enable"},
+    s = {":Copilot status<CR>", "status"}
+  }
+})
+
+-- Ugaterm
+wk.register({
+  t = {
+    name = "ugaterm",
+    t = {':UgatermToggle<CR>', 'UgatermToggle'},
+    n = {'<cmd>UgatermNew<CR>', 'UgatermNew'},
+    d = {'<cmd>UgatermDelete<CR>', 'UgatermDelete'},
+    s = {'<cmd>UgatermSelect<CR>', 'UgatermSelect'},
+  }
+}, { prefix = "<leader>"})
+
+
+-- Telescope undo
+map("n", "<leader>su", "<cmd>Telescope undo<cr>", { desc = "Telescope undo" })
