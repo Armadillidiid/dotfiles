7a090dca2f3923b8e7ea8592c10b840c132ecf3d
Emmanuel Isenah <emmanuelisenah@gmail.com>
2023-04-28 19:45:24 +0100

    Added my leetcode fork

--- LazyVim/.config/LazyVim/lua/config/options.lua
+++ LazyVim/.config/LazyVim/lua/config/options.lua
@@ -6,3 +6,8 @@
 vim.cmd('highlight LeapLabelPrimary guifg=#fc0352 guibg=none')
 vim.cmd('highlight LeapLabelSecondary guifg=cyan guibg=none')
 vim.cmd('highlight LeapBackdrop guifg=grey')
+
+-- Set the browser to open the leetcode problem in
+vim.g.leetcode_browser = "firefox"
+vim.g.leetcode_solution_filetype = "typescript"
+vim.g.leetcode_hide_paid_only = 1
---
+++ LazyVim/.config/LazyVim/lua/plugins/leetcode.lua
@@ -0,0 +1,3 @@
+return {
+"Armadillidiid/leetcode.vim",
+}
