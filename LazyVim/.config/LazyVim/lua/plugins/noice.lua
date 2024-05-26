74e37cf6b96c0f590935fa2cfd94dcb561607ddd
Emmanuel Isenah <emmanuelisenah@gmail.com>
2024-05-26 15:55:01 +0100

    disable noice notification for neo-tree

--- LazyVim/.config/LazyVim/lua/plugins/noice.lua
+++ LazyVim/.config/LazyVim/lua/plugins/noice.lua
@@ -2,9 +2,16 @@ return {
   "folke/noice.nvim",
   opts = function(_, opts)
     table.insert(opts.routes, {
+      view = "mini",
       filter = {
         event = "notify",
         find = "No information available",
+        any = {
+          { find = "hidden" },
+          { find = "clipboard" },
+          { find = "Deleted" },
+          { find = "Renamed" },
+        },
       },
       opts = { skip = true },
     })
