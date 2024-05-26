ca968f2e44050e9b4197ba620beda90be4ae0fa9
Emmanuel Isenah <emmanuelisenah@gmail.com>
2024-05-26 16:44:02 +0100

    fix

--- LazyVim/.config/LazyVim/lua/plugins/noice.lua
+++ LazyVim/.config/LazyVim/lua/plugins/noice.lua
@@ -5,13 +5,18 @@ return {
       view = "mini",
       filter = {
         event = "notify",
-        find = "No information available",
         any = {
           { find = "hidden" },
           { find = "clipboard" },
           { find = "Deleted" },
           { find = "Renamed" },
         },
+      },
+    })
+    table.insert(opts.routes, {
+      filter = {
+        event = "notify",
+        find = "No information available",
       },
       opts = { skip = true },
     })
