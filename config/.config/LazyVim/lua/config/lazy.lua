ddfd5902e55118357c3c5089c0fc9bd15a47e5d6
Emmanuel Isenah <emmanuelisenah@gmail.com>
2026-05-21 12:44:23 +0100

    update

--- config/.config/LazyVim/lua/config/lazy.lua
+++ config/.config/LazyVim/lua/config/lazy.lua
@@ -41,7 +41,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.ai.copilot" },
     { import = "lazyvim.plugins.extras.ai.sidekick" },
     { import = "lazyvim.plugins.extras.ai.copilot-chat" },
-    { import = "lazyvim.plugins.extras.formatting.biome" },
+    { import = "lazyvim.plugins.extras.lang.typescript.biome" },
     { import = "lazyvim.plugins.extras.editor.aerial" },
     { import = "lazyvim.plugins.extras.editor.dial" },
     { import = "lazyvim.plugins.extras.editor.inc-rename" },
